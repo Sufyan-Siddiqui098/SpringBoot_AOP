a2d517512e80c294c2e595edcbb946fb50afd27e
Sufyan Siddiqui <sufyansiddiqui098@gmail.com>
2025-09-28 15:55:36 +0500

    learned and write pointcut expression

--- src/main/java/io/datajek/springaop/movie_recommender_aop/aspect/AccessCheckAspect.java
+++ src/main/java/io/datajek/springaop/movie_recommender_aop/aspect/AccessCheckAspect.java
@@ -12,10 +12,16 @@ import org.springframework.context.annotation.Configuration;
 public class AccessCheckAspect {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @Before("execution(* io.datajek.springaop.movie_recommender_aop.business.*.*(..))")
-    public void userAccess(JoinPoint joinPoint){
+//    @Before("execution(* io.datajek.springaop.movie_recommender_aop.business.*.*(..))")
+//    public void userAccess(JoinPoint joinPoint){
+//        logger.info("Intercepted call before execution: {}", joinPoint );
+//    }
+
+    //@Before("execution(* io.datajek.springaop.movie_recommender_aop..*.*(..))") // intercept all packages' all classes
+    //  @Before("execution(String io.datajek.springaop.movie_recommender_aop..*.*(..))") // intercept which returns String
+   // @Before("execution(* io.datajek.springaop.movie_recommender_aop..*.*(String,..)) ||" +
+   //         " execution(String io.datajek.springaop.movie_recommender_aop..*.*(..))") // intercept which return String or have String as 1st argument
+    public void before(JoinPoint joinPoint){
         logger.info("Intercepted call before execution: {}", joinPoint );
     }
-
-
 }
